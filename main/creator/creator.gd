b76d10a9c52abe6ecf5ab6bfca0c4aaec45233b3
Connor Carey <cocarey@Connors-Android-Laptop.local>
2024-09-14 14:18:06 -0400

    Fixed function call bug and added variable clarity

--- main/creator/creator.gd
+++ main/creator/creator.gd
@@ -3,26 +3,31 @@ class_name Creator extends HTTPRequest
 var fluid_type = preload("res://main/word_object/fluid/fluid.tscn")
 var inanimate_solid_type = preload("res://main/word_object/inanimate_solid/inanimate_solid.tscn")
 var life_type = preload("res://main/word_object/life/life.tscn")
+	
 
 func create_object(word: String) -> WordObject:
 	var ai_response: Dictionary = {}
 	var word_obj: WordObject = null
+	if "type" not in ai_response: return null
 	
 	# Instantiating and adding type specific properties to the word object
-	if ai_response["type"] == "fluid":
-		word_obj = fluid_type.instantiate()
-		configure_fluid(word_obj, ai_response)
-	elif ai_response["type"] == "inanimate_solid":
-		word_obj = inanimate_solid_type.instantiate()
-		configure_fluid(word_obj, ai_response)
-	elif ai_response["type"] == "life":
-		word_obj = life_type.instantiate()
-		configure_fluid(word_obj, ai_response)
-	else:
-		return null
+	match ai_response["type"]:
+		"fluid":
+			word_obj = fluid_type.instantiate()
+			configure_fluid(word_obj, ai_response)
+		"inanimate_solid":
+			word_obj = inanimate_solid_type.instantiate()
+			configure_inanimate_object(word_obj, ai_response)
+		"life":
+			word_obj = life_type.instantiate()
+			configure_life(word_obj, ai_response)
+
 	
 	# Adding non-type specific properties
 	for property in ai_response:
+		var value = ai_response[property]
+		
+		# Non-specific properties
 		if property == "text":
 			word_obj.text = ai_response["text"]
 		elif property == "dynamic":
@@ -34,19 +39,28 @@ func create_object(word: String) -> WordObject:
 		
 	return word_obj
 
+
 # Adding fluid specific properties
 func configure_fluid(obj: WordObject, ai_response: Dictionary):
 	for property in ai_response:
+		var value = ai_response[property]
+		
 		pass
 
 # Adding inanimate object specific properties
 func configure_inanimate_object(obj: WordObject, ai_response: Dictionary):
 	for property in ai_response:
+		var value = ai_response[property]
+		
+		# Type-specific properties
 		if property == "physics_type":
-			obj.physics_type = ai_response["physics_type"]
+			obj.physics_type = value
 
 # Adding life specific properties
 func configure_life(obj: WordObject, ai_response: Dictionary):
 	for property in ai_response:
+		var value = ai_response[property]
+		
+		# Type-specific properties
 		if property == "movement_type":
-			obj.movement_type = ai_response["movement_type"]
+			obj.movement_type = value
