1467d5eb50191e29ae16db9e9390c6518617575b
kiwijuice56 <alfaroeric127@gmail.com>
2024-09-14 16:42:07 -0400

    stuffs

--- main/level/level_manager.gd
+++ main/level/level_manager.gd
@@ -1,1 +1,15 @@
 class_name LevelManager extends Node
+
+@export var levels: Array[PackedScene]
+
+var current_id: int = 0
+var loaded_level: Level
+
+func _ready() -> void:
+	load_level(0)
+
+func load_level(id: int) -> void:
+	if is_instance_valid(loaded_level):
+		loaded_level.queue_free()
+	loaded_level = levels[id].instantiate()
+	add_child(loaded_level)
