8a5517508cec8f32bb9eaee733cdaf5e1a27ada9
kiwijuice56 <alfaroeric127@gmail.com>
2024-09-14 12:12:28 -0400

    Basic player movement

--- main/player/player.gd
+++ main/player/player.gd
@@ -1,1 +1,20 @@
-class_name Playaer extends CharacterBody2D
+class_name Player extends CharacterBody2D
+# https://kidscancode.org/godot_recipes/4.x/2d/platform_character/index.html
+
+@export var speed = 1200
+@export var jump_speed = -1800
+@export var gravity = 4000
+@export_range(0.0, 1.0) var friction = 0.1
+@export_range(0.0 , 1.0) var acceleration = 0.25
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	var dir = Input.get_axis("ui_left", "ui_right")
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, friction)
+
+	move_and_slide()
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_speed
