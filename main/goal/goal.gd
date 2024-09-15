0af8b7864d4bc7945e822878ab99f2e4ff73442b
kiwijuice56 <alfaroeric127@gmail.com>
2024-09-15 09:30:06 -0400

    seemstowork fine

--- main/goal/goal.gd
+++ main/goal/goal.gd
@@ -1,8 +1,16 @@
 class_name Goal extends Area2D
 
+var fix: bool = false
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	$Timer.timeout.connect(_on_timeout)
+
+func _on_timeout() -> void:
+	fix = true
 
 func _on_body_entered(body: Node2D) -> void:
+	if not fix:
+		return
 	if body.can_move:
 		Ref.level_manager.end_level.call_deferred()
